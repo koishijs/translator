06e1d5e7e128794cbba50dd7f75f8d428a2f2b95
Maiko Sinkyaet Tan <maiko.tan.coding@gmail.com>
2022-11-13 10:03:44 +0000

    feat: implement baidu translator prototype

---
+++ packages/baidu/readme.md
@@ -0,0 +1,28 @@
+# koishi-plugin-translator-baidu
+
+[![npm](https://img.shields.io/npm/v/koishi-plugin-baidu?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-translator-baidu)
+
+在 Koishi 中使用 [百度翻译](https://fanyi.baidu.com/)。
+
+## 配置项
+
+### appid
+
+- 类型: `string`
+
+百度翻译的 AppID。
+
+### secret
+
+- 类型: `string`
+
+有道翻译的 Secret。
+
+## 指令：translate
+
+支持的语言名包括 zh, en, yue, wyw, jp, kor, fra, spa, th, ara, ru, pt, de, it, el, nl, pl, bul, est, dan, fin, cs, rom, slo, swe, hu, cht, vie。
+
+- 基本语法：`translate <text>`
+- 选项列表：
+  - -f, --from \<lang>  源语言
+  - -t, --to \<lang>  目标语言
---
+++ packages/baidu/src/index.ts
@@ -0,0 +1,50 @@
+import { createHash } from 'crypto'
+import { Context, Schema } from 'koishi'
+import Translator from '@koishijs/translator'
+
+class BaiduTranslator extends Translator {
+  constructor(ctx: Context, public config: BaiduTranslator.Config) {
+    super(ctx, config)
+  }
+
+  private _signature(query: string, salt: string | number) {
+    // https://fanyi-api.baidu.com/doc/21
+    const str = this.config.appid + query + salt + this.config.secret
+    const md5 = createHash('md5')
+    md5.update(str)
+    return md5.digest('hex')
+  }
+
+  async translate(input: string, options?: Translator.Options): Promise<string> {
+    const from = options.source || ''
+    const to = options.target || 'zh'
+    const salt = new Date().getTime()
+    const sign = this._signature(input, salt.toString())
+
+    const resp = await this.ctx.http.post(
+      'http://api.fanyi.baidu.com/api/trans/vip/translate',
+      { from, to, q: input, salt, appid: this.config.appid, sign },
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    )
+
+    return resp.trans_result[0].dst
+  }
+}
+
+namespace BaiduTranslator {
+  export interface Config extends Translator.Config {
+    appid: string
+    secret: string
+  }
+
+  export const Config: Schema<Config> = Schema.object({
+    appid: Schema.string().required().description('百度翻译的 App ID。'),
+    secret: Schema.string().role('secret').required().description('百度翻译的 Secret。'),
+  })
+}
+
+export default BaiduTranslator
